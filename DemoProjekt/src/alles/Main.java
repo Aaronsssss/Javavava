4b385348bfacd8da63a16740dff3e9f917783b5a
aaron.swann <aaron.swann@B206-22.BK.local>
2021-11-24 09:08:43 +0100

    Moin hier ist der erste Commit

---
+++ DemoProjekt/src/alles/Main.java
@@ -0,0 +1,18 @@
+package alles;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main main = new Main();
+		main.testMethode();
+
+	}
+	
+	public void testMethode() {
+		for(int i = 0; i<10; i++) {
+			System.out.println("Hier steht die " + (i+1) + ". Zeile!");
+		}
+	}
+
+}
